57eb723eef92ec3962f3af42fe2090274064045c
Filipe Reis <filipe.reis17@live.com>
2023-07-12 17:03:48 -0400

    GameDTO

--- src/main/java/com/devsuperior/dslist/services/GameService.java
+++ src/main/java/com/devsuperior/dslist/services/GameService.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.devsuperior.dslist.dto.GameDTO;
 import com.devsuperior.dslist.dto.GameMinDTO;
 import com.devsuperior.dslist.entities.Game;
 import com.devsuperior.dslist.repositories.GameRepository;
@@ -14,6 +15,11 @@ public class GameService {
 
 	@Autowired
 	private GameRepository gameRepository;
+	
+	public GameDTO findById(Long id) {
+		Game result = gameRepository.findById(id).get();
+		return new GameDTO(result);		
+	}
 	
 	public List<GameMinDTO> findAll() {
 		List<Game> result = gameRepository.findAll();
